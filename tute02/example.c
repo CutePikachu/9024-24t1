f2bc4c1098c5df81cfb124ed2a9d023cf8f59a48
Tina Ji <z5191511@ad.unsw.edu.au>
2024-02-20 16:56:43 +1100

    update

--- tute02/example.c
+++ tute02/example.c
@@ -4,3 +4,33 @@ int main(int argc, char *argv[]) {
     printf("hello world");
     return 1;
 }
+// name - first name last name
+
+typedef struct {
+  char firstname[21];
+  char lastname[21];
+} Name;
+
+typedef char[11] PhoneNumber;
+
+typedef struct {
+  short streetNumber;
+  char streenName[31];
+  char suburb[16];
+  char state[4];
+  short postcode;
+} Address;
+
+typedef struct {
+  short day;
+  short month;
+  short year;
+} DoB;
+
+typedef struct {
+  Name name;
+  Address address;
+  DoB dob;
+  PhoneNumber phone;
+  Char email[31];
+} Contact;
